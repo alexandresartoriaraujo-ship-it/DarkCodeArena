4e566ae01224f748534b47f60ae3ce22e9281847
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-05-14 10:59:06 -0300

    subindo

---
+++ ex10.js
@@ -0,0 +1,9 @@
+// Soma dos pares
+
+let soma = 0
+console.log("====")
+for(let i = 2; i < 101; i+=2 ){
+    soma = soma + i
+}
+console.log(` Os números pares entre 1 e 100 é: ${soma}`)
+console.log("====")
