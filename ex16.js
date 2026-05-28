ba240e7ef78f6581ef204727a289d00fade39d19
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-05-28 11:00:46 -0300

    subindo

--- ex16.js
+++ ex16.js
@@ -5,3 +5,13 @@ console.log("")
 console.log(" ==== Porcentagem de acertos ==== ")
 console.log("")
 let qtq = readline.question("Qual foi a quantidade total: ")
+console.log("")
+let qc = readline.questionFloat("Quantidade de acertos é: ")
+let p = (qc / qtq) * 100
+if(qtq > 0){
+    console.log("")
+    console.log(`Seu percentual foi de: ${p.toFixed(2)}%`)
+    console.log("")
+}else{
+    console.log("Erro: O total de questões deve ser maior que zero. ")
+}
