e62a4096d3884834bb651f9ae13e65aa55d7b9cc
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-05-28 09:59:27 -0300

    subindo

---
+++ ex16.js
@@ -0,0 +1,7 @@
+//Porcentagem de Acertos
+
+const readline = require("readline-sync")
+console.log("")
+console.log(" ==== Porcentagem de acertos ==== ")
+console.log("")
+let qtq = readline.question("Qual foi a quantidade total: ")
