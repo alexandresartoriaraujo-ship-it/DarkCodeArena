3946fc8b8cd12a97cf9614783dcfdb4a8be977b7
alexandresartoriaraujo-ship-it <alexandre.sartoriaraujo@gmail.com>
2026-05-28 11:11:47 -0300

    subindo

---
+++ ex19.js
@@ -0,0 +1,10 @@
+let t = 50
+let c = 30
+let p = 50*30/100
+console.log("==== Consumo de combustivel ====")
+console.log("")
+console.log(` total de combustivel ${t}L`)
+console.log("")
+console.log(` Litros consumidos ${c}%`)
+console.log("")
+console.log(` Quanto resta ${p}L`)
